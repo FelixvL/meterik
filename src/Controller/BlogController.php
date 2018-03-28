b1a750cd5e9412de11f5354a15d97338f91fbc21
Felixvl <digitaleservice@gmail.com>
2018-03-28 12:51:03 +0200

    initieel werkend

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,36 @@
+<?php
+// src/Controller/BlogController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class BlogController extends Controller
+{
+    /**
+     * Matches /blog exactly
+     *
+     * @Route("/blog", name="blog_list")
+     */
+    public function list()
+    {
+        $number = 24;
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+    /**
+     * Matches /blog/*
+     *
+     * @Route("/blog/{slug}", name="blog_show")
+     */
+    public function show($slug)
+    {
+        // $slug will equal the dynamic part of the URL
+        // e.g. at /blog/yay-routing, then $slug='yay-routing'
+
+        // ...
+    }
+}
